e7bb3aa6a60fa4c0dbd1a5187c4f30ffaef82255
AJ1238 <abigailjamieson532@gmail.com>
2026-03-16 21:48:55 -0500

    Add files via upload

---
+++ HomePage.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace TrajjRentalFrm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.hOMEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.bOOKINGSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.aBOUTUSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.cONTACTUSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.mYACCOUNTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.lblTrajj = new System.Windows.Forms.Label();
+			this.vEHICLESToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hOMEToolStripMenuItem,
+            this.bOOKINGSToolStripMenuItem,
+            this.aBOUTUSToolStripMenuItem,
+            this.cONTACTUSToolStripMenuItem,
+            this.mYACCOUNTToolStripMenuItem,
+            this.vEHICLESToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// hOMEToolStripMenuItem
+			// 
+			this.hOMEToolStripMenuItem.Name = "hOMEToolStripMenuItem";
+			this.hOMEToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+			this.hOMEToolStripMenuItem.Text = "HOME";
+			// 
+			// bOOKINGSToolStripMenuItem
+			// 
+			this.bOOKINGSToolStripMenuItem.Name = "bOOKINGSToolStripMenuItem";
+			this.bOOKINGSToolStripMenuItem.Size = new System.Drawing.Size(91, 20);
+			this.bOOKINGSToolStripMenuItem.Text = "RESERVATION";
+			// 
+			// aBOUTUSToolStripMenuItem
+			// 
+			this.aBOUTUSToolStripMenuItem.Name = "aBOUTUSToolStripMenuItem";
+			this.aBOUTUSToolStripMenuItem.Size = new System.Drawing.Size(74, 20);
+			this.aBOUTUSToolStripMenuItem.Text = "ABOUT US";
+			// 
+			// cONTACTUSToolStripMenuItem
+			// 
+			this.cONTACTUSToolStripMenuItem.Name = "cONTACTUSToolStripMenuItem";
+			this.cONTACTUSToolStripMenuItem.Size = new System.Drawing.Size(89, 20);
+			this.cONTACTUSToolStripMenuItem.Text = "CONTACT US";
+			// 
+			// mYACCOUNTToolStripMenuItem
+			// 
+			this.mYACCOUNTToolStripMenuItem.Name = "mYACCOUNTToolStripMenuItem";
+			this.mYACCOUNTToolStripMenuItem.Size = new System.Drawing.Size(96, 20);
+			this.mYACCOUNTToolStripMenuItem.Text = "MY ACCOUNT";
+			// 
+			// lblTrajj
+			// 
+			this.lblTrajj.AutoSize = true;
+			this.lblTrajj.Font = new System.Drawing.Font("Palatino Linotype", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTrajj.Location = new System.Drawing.Point(144, 48);
+			this.lblTrajj.Name = "lblTrajj";
+			this.lblTrajj.Size = new System.Drawing.Size(488, 49);
+			this.lblTrajj.TabIndex = 1;
+			this.lblTrajj.Text = "TRAJJ RENTAL COMPANY";
+			// 
+			// vEHICLESToolStripMenuItem
+			// 
+			this.vEHICLESToolStripMenuItem.Name = "vEHICLESToolStripMenuItem";
+			this.vEHICLESToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+			this.vEHICLESToolStripMenuItem.Text = "VEHICLES";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.lblTrajj);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem hOMEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bOOKINGSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aBOUTUSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cONTACTUSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mYACCOUNTToolStripMenuItem;
+        private System.Windows.Forms.Label lblTrajj;
+        private System.Windows.Forms.ToolStripMenuItem vEHICLESToolStripMenuItem;
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace TRAJJ_Company
+namespace TrajjRentalFrm
 {
     internal static class Program
     {
@@ -16,7 +16,7 @@ namespace TRAJJ_Company
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new lbltrajLoginFrm());
+            Application.Run(new HomePage());
         }
     }
 }
