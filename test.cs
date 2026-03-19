c6c81ef26ca92f0abf05d2e8944ca892e4a20eea
Troxel29 <troxelorboine@gmail.com>
2026-03-19 16:23:18 -0500

    test2

--- test.cs
+++ test.cs
@@ -7,6 +7,26 @@ using System.Threading.Tasks;
 namespace TRAJJ_Company
 {
     internal class test
-    {
+    {#region test
+        public void test1()
+        {
+            Console.WriteLine("test1");
+        }
+        public void test2()
+        {
+            Console.WriteLine("test2");
+        }
+        public void test3()
+        {
+            Console.WriteLine("test3");
+        }
+        public void test4()
+        {
+            Console.WriteLine("test4");
+        }
+        public void test5()
+        {
+            Console.WriteLine("test5");
+        }
     }
 }
